843982e43d583bccd86f6a5226327373621f41a7
ammar prasla <apa105@sfu.ca>
2024-10-03 22:04:15 -0700

    UserSearchHistory_Update

---
+++ config/initializers/cas.rb
@@ -0,0 +1,4 @@
+CASClient::Frameworks::Rails::Filter.configure(
+  cas_base_url: 'https://cas.sfu.ca/cas', 
+  service_url: 'https://cas.plan.sfu.ca/cas',  #temporary service url
+)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
   get "/articles", to: "articles#index"
   get "/courses/search_page", to: "courses#search_page"
+  
 
   # Route to handle the search request from the front end
   get "/courses/search", to: "courses#search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/cas_sessions/new", to: "cas_sessions#new", as: :login # Route to initiate CAS login
+  get "/cas_sessions/create", to: "cas_sessions#create"       # CAS callback after login
+  delete "/cas_sessions/destroy", to: "cas_sessions#destroy", as: :logout
 
+  # UserSearchHistory routes
+  resources :search_histories, only: [:index]
+
+  resources :users, only: [:show, :edit, :update, :destroy]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
