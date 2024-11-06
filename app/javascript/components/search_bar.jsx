72c9369b5816755abc563b4b92b8d42bba0b717d
Armand Baril <amb35@sfu.ca>
2024-11-06 00:38:03 -0800

    minor ui feel improvements

--- app/javascript/components/search_bar.jsx
+++ app/javascript/components/search_bar.jsx
@@ -5,7 +5,7 @@ import "./search_bar.css"
 // a component for a search bar with a search icon
 export const SearchBar = ({ onChange=()=>{} }) => (
     <div className="search_bar">
-        <input type="text" className="search_bar_input" placeholder="Search" name="search" onBlur={(event) => onChange(event.target.value)}></input>
+        <input type="search" className="search_bar_input" placeholder="Search" name="search" onBlur={(event) => onChange(event.target.value)}></input>
         {/*render search icon*/}
         <Search />
     </div>
--- app/javascript/components/search_bar_and_dropdown.jsx
+++ app/javascript/components/search_bar_and_dropdown.jsx
@@ -4,6 +4,7 @@ import { defaultQuery } from "./searchCourses.js";
 import { SearchBar } from "./search_bar.jsx";
 import { Checkbox } from "./checkbox.jsx";
 import { changeQueryCallback, UpdateTermsCallback, UpdateSessionCallback } from "./callback.js";
+import { FoldingPanel } from "./folding_panel.jsx";
 
 export const all_categories = {
     search_in_props: ["title", "description", "instructors", "year", "term"],
@@ -117,161 +118,161 @@ const SearchBarWithDropdown = () => {
       <div className="horizontal-stack">
         <SearchBar id="course_search" name="course_search" onChange={handleSearchChange} />
         <div className="padding_medium"></div>
-        <button id="advanced_search_button" onClick={toggleDropdown}>
-          Advanced Search
-        </button>
-      </div>
-      {showDropdown && (
         <div>
-          <div className="dropdown_menu">
-            <div>
-              <h3>Search in</h3>
-              <Checkbox
-                id="search_in_title"
-                label="title"
-                defaultChecked={matchesSearch(
-                  defaultQuery.search_in_props,
-                  "title"
-                )}
-                onChange={(checked) =>
-                  setSearchParam("search_in_props", "title", checked)
-                }
-              />
-              <Checkbox
-                id="search_in_description"
-                label="description"
-                defaultChecked={matchesSearch(
-                  defaultQuery.search_in_props,
-                  "description"
-                )}
-                onChange={(checked) =>
-                  setSearchParam("search_in_props", "description", checked)
-                }
-              />
-              <Checkbox
-                id="search_in_instructors"
-                label="instructors"
-                defaultChecked={matchesSearch(
-                  defaultQuery.search_in_props,
-                  "instructors"
-                )}
-                onChange={(checked) =>
-                  setSearchParam("search_in_props", "instructors", checked)
-                }
-              />
-              <Checkbox
-                id="search_in_year"
-                label="year"
-                defaultChecked={matchesSearch(
-                  defaultQuery.search_in_props,
-                  "year"
-                )}
-                onChange={(checked) =>
-                  setSearchParam("search_in_props", "year", checked)
-                }
-              />
-              <Checkbox
-                id="search_in_term"
-                label="term"
-                defaultChecked={matchesSearch(
-                  defaultQuery.search_in_props,
-                  "term"
-                )}
-                onChange={(checked) =>
-                  setSearchParam("search_in_props", "term", checked)
-                }
-              />
-            </div>
-            <div>
-              <h3>Departments</h3>
-              <Checkbox
-                id="dept_math"
-                label="math"
-                defaultChecked={matchesSearch(defaultQuery.departments, "math")}
-                onChange={(checked) =>
-                  setSearchParam("departments", "math", checked)
-                }
-              />
-              <Checkbox
-                id="dept_cmpt"
-                label="cmpt"
-                defaultChecked={matchesSearch(defaultQuery.departments, "cmpt")}
-                onChange={(checked) =>
-                  setSearchParam("departments", "cmpt", checked)
-                }
-              />
-              <Checkbox
-                id="dept_macm"
-                label="macm"
-                defaultChecked={matchesSearch(defaultQuery.departments, "macm")}
-                onChange={(checked) =>
-                  setSearchParam("departments", "macm", checked)
-                }
-              />
-              <Checkbox
-                id="dept_ensc"
-                label="ensc"
-                defaultChecked={matchesSearch(defaultQuery.departments, "ensc")}
-                onChange={(checked) =>
-                  setSearchParam("departments", "ensc", checked)
-                }
-              />
-              <Checkbox
-                id="dept_phys"
-                label="phys"
-                defaultChecked={matchesSearch(defaultQuery.departments, "phys")}
-                onChange={(checked) =>
-                  setSearchParam("departments", "phys", checked)
-                }
-              />
-              <Checkbox
-                id="dept_chem"
-                label="chem"
-                defaultChecked={matchesSearch(defaultQuery.departments, "chem")}
-                onChange={(checked) =>
-                  setSearchParam("departments", "chem", checked)
-                }
-              />
-              <Checkbox
-                id="dept_stat"
-                label="stat"
-                defaultChecked={matchesSearch(defaultQuery.departments, "stat")}
-                onChange={(checked) =>
-                  setSearchParam("departments", "stat", checked)
-                }
-              />
-            </div>
+          <button id="advanced_search_button" onClick={toggleDropdown}>
+            Advanced Search
+          </button>
+          <FoldingPanel className="search_dropdown" is_open={showDropdown} set_open_callback={setShowDropdown}>
+            <div className="dropdown_menu">
+              <div>
+                <h3>Search in</h3>
+                <Checkbox
+                  id="search_in_title"
+                  label="title"
+                  defaultChecked={matchesSearch(
+                    defaultQuery.search_in_props,
+                    "title"
+                  )}
+                  onChange={(checked) =>
+                    setSearchParam("search_in_props", "title", checked)
+                  }
+                />
+                <Checkbox
+                  id="search_in_description"
+                  label="description"
+                  defaultChecked={matchesSearch(
+                    defaultQuery.search_in_props,
+                    "description"
+                  )}
+                  onChange={(checked) =>
+                    setSearchParam("search_in_props", "description", checked)
+                  }
+                />
+                <Checkbox
+                  id="search_in_instructors"
+                  label="instructors"
+                  defaultChecked={matchesSearch(
+                    defaultQuery.search_in_props,
+                    "instructors"
+                  )}
+                  onChange={(checked) =>
+                    setSearchParam("search_in_props", "instructors", checked)
+                  }
+                />
+                <Checkbox
+                  id="search_in_year"
+                  label="year"
+                  defaultChecked={matchesSearch(
+                    defaultQuery.search_in_props,
+                    "year"
+                  )}
+                  onChange={(checked) =>
+                    setSearchParam("search_in_props", "year", checked)
+                  }
+                />
+                <Checkbox
+                  id="search_in_term"
+                  label="term"
+                  defaultChecked={matchesSearch(
+                    defaultQuery.search_in_props,
+                    "term"
+                  )}
+                  onChange={(checked) =>
+                    setSearchParam("search_in_props", "term", checked)
+                  }
+                />
+              </div>
+              <div>
+                <h3>Departments</h3>
+                <Checkbox
+                  id="dept_math"
+                  label="math"
+                  defaultChecked={matchesSearch(defaultQuery.departments, "math")}
+                  onChange={(checked) =>
+                    setSearchParam("departments", "math", checked)
+                  }
+                />
+                <Checkbox
+                  id="dept_cmpt"
+                  label="cmpt"
+                  defaultChecked={matchesSearch(defaultQuery.departments, "cmpt")}
+                  onChange={(checked) =>
+                    setSearchParam("departments", "cmpt", checked)
+                  }
+                />
+                <Checkbox
+                  id="dept_macm"
+                  label="macm"
+                  defaultChecked={matchesSearch(defaultQuery.departments, "macm")}
+                  onChange={(checked) =>
+                    setSearchParam("departments", "macm", checked)
+                  }
+                />
+                <Checkbox
+                  id="dept_ensc"
+                  label="ensc"
+                  defaultChecked={matchesSearch(defaultQuery.departments, "ensc")}
+                  onChange={(checked) =>
+                    setSearchParam("departments", "ensc", checked)
+                  }
+                />
+                <Checkbox
+                  id="dept_phys"
+                  label="phys"
+                  defaultChecked={matchesSearch(defaultQuery.departments, "phys")}
+                  onChange={(checked) =>
+                    setSearchParam("departments", "phys", checked)
+                  }
+                />
+                <Checkbox
+                  id="dept_chem"
+                  label="chem"
+                  defaultChecked={matchesSearch(defaultQuery.departments, "chem")}
+                  onChange={(checked) =>
+                    setSearchParam("departments", "chem", checked)
+                  }
+                />
+                <Checkbox
+                  id="dept_stat"
+                  label="stat"
+                  defaultChecked={matchesSearch(defaultQuery.departments, "stat")}
+                  onChange={(checked) =>
+                    setSearchParam("departments", "stat", checked)
+                  }
+                />
+              </div>
 
-            <div>
-              <h3>Levels</h3>
-              <Checkbox
-                id="level_1"
-                label="1xx"
-                defaultChecked={matchesSearch(defaultQuery.levels, "1xx")}
-                onChange={(checked) => setSearchParam("levels", "1xx", checked)}
-              />
-              <Checkbox
-                id="level_2"
-                label="2xx"
-                defaultChecked={matchesSearch(defaultQuery.levels, "2xx")}
-                onChange={(checked) => setSearchParam("levels", "2xx", checked)}
-              />
-              <Checkbox
-                id="level_3"
-                label="3xx"
-                defaultChecked={matchesSearch(defaultQuery.levels, "3xx")}
-                onChange={(checked) => setSearchParam("levels", "3xx", checked)}
-              />
-              <Checkbox
-                id="level_4"
-                label="4xx"
-                defaultChecked={matchesSearch(defaultQuery.levels, "4xx")}
-                onChange={(checked) => setSearchParam("levels", "4xx", checked)}
-              />
+              <div>
+                <h3>Levels</h3>
+                <Checkbox
+                  id="level_1"
+                  label="1xx"
+                  defaultChecked={matchesSearch(defaultQuery.levels, "1xx")}
+                  onChange={(checked) => setSearchParam("levels", "1xx", checked)}
+                />
+                <Checkbox
+                  id="level_2"
+                  label="2xx"
+                  defaultChecked={matchesSearch(defaultQuery.levels, "2xx")}
+                  onChange={(checked) => setSearchParam("levels", "2xx", checked)}
+                />
+                <Checkbox
+                  id="level_3"
+                  label="3xx"
+                  defaultChecked={matchesSearch(defaultQuery.levels, "3xx")}
+                  onChange={(checked) => setSearchParam("levels", "3xx", checked)}
+                />
+                <Checkbox
+                  id="level_4"
+                  label="4xx"
+                  defaultChecked={matchesSearch(defaultQuery.levels, "4xx")}
+                  onChange={(checked) => setSearchParam("levels", "4xx", checked)}
+                />
+              </div>
             </div>
-          </div>
+          </FoldingPanel>
         </div>
-      )}
+      </div>
     </div>
     </>
   );
