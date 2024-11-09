662daa5d91df109d10e330662a021fd8709bfa1d
ammar prasla <apa105@sfu.ca>
2024-11-09 00:51:40 -0800

    few authentication updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  # commented to allow lighthouse
-  # allow_browser versions: :modern
+  before_action :set_current_user
 
   private 
 
   def authenticate_user!
-    redirect_to root_path, alert: "You must be logged in to do that." unless user_signed_in?
+    redirect_to login_path, alert: "You must be logged in to do that." unless user_signed_in?
   end
 
   def current_user
@@ -29,43 +27,14 @@ class ApplicationController < ActionController::Base
     session[:user_id] = user.id
   end
 
-  def logout(user)
+  def logout
     Current.user = nil
     reset_session
   end
 
-end
-
-=begin
-
-JUST REMOVE "=begin" AND "=end" TO REMOVE COMMENTS!
-
-  before_action :set_current_user
-  before_action :authenticate_user!
-
-  # Set the current user based on CAS session data
   def set_current_user
-    if session[:cas_user]
-      Rails.logger.info "Session CAS user: #{session[:cas_user]}"  # Log session user
-      @current_user = User.find_by(user_id: session[:cas_user])  # Using user_id instead of email
-      Rails.logger.info "Current User: #{@current_user&.user_id}"   # Log if current user found
-    end
-  end
-
-  # Helper method to access the current user
-  def current_user
-    @current_user
-  end
-
-  # Require user to be authenticated before accessing certain actions
-  def authenticate_user!
-    unless current_user
-      Rails.logger.info "User not authenticated. Redirecting to login."  # Log when redirecting to login
-      redirect_to login_path
-    else
-      Rails.logger.info "User authenticated as #{current_user.user_id}"   # Log authenticated user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
     end
   end
 end
-
-=end
